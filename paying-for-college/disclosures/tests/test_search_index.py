07e5a7a7a9f89a4b8345bc32f2e79444888a43a8
higs4281 <higs4281@gmail.com>
2015-11-08 14:31:07 -0500

    add tests for search_indexes.py

---
+++ paying-for-college/disclosures/tests/test_search_index.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+import json
+
+from django.test import TestCase
+from disclosures.search_indexes import SchoolIndex, School
+
+
+class SchoolIndexTest(TestCase):
+
+    fixtures = ['test_fixture.json']
+    MOCK_INDEX = SchoolIndex()
+    mock_obj = School.objects.get(pk=155317)
+
+    def test_index(self):
+        self.assertTrue(self.MOCK_INDEX.get_model() == School)
+        self.assertTrue(self.MOCK_INDEX.index_queryset().count() ==
+                        School.objects.count())
+        self.assertTrue('Jayhawks' in
+                        self.MOCK_INDEX.prepare_autocomplete(self.mock_obj))
