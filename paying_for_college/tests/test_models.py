2b9cb5075f75a232ab7c127fa82b614f2d8f690c
higs4281 <higs4281@gmail.com>
2015-11-18 12:32:14 -0500

    adding, updating tests

--- paying_for_college/tests/test_models.py
+++ paying_for_college/tests/test_models.py
@@ -12,10 +12,12 @@ class SchoolAliasTest(TestCase):
 
     def create_school(self, ID=999999,
                       data_json='',
+                      accreditor="Almighty Wizard",
                       city="Emerald City",
                       state="OZ"):
         return School.objects.create(school_id=ID,
                                      data_json=data_json,
+                                     accreditor=accreditor,
                                      city=city,
                                      state=state)
 
@@ -56,9 +58,10 @@ class SchoolAliasTest(TestCase):
         p = self.create_program(s)
         self.assertTrue(isinstance(p, Program))
         self.assertTrue(p.program_name in p.__unicode__())
+        self.assertTrue(print_vals(s) is None)
         self.assertTrue("Emerald City" in print_vals(s, val_list=True))
         self.assertTrue("Emerald City" in print_vals(s, val_dict=True)['city'])
-        self.assertTrue(print_vals(s) is None)
+        self.assertTrue("Emerald City" in print_vals(s, noprint=True))
 
     def test_constant_models(self):
         cr = ConstantRate(name='cr test', slug='crTest', value='0.1')
