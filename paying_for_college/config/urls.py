a64f3c4c5f679aa2fc5006564618ef24e5444e37
higs4281 <higs4281@gmail.com>
2015-11-17 20:39:57 -0500

    fix setups after traumatic reboot

--- paying_for_college/config/urls.py
+++ paying_for_college/config/urls.py
@@ -2,20 +2,35 @@ from django.conf.urls import url, include
 from django.conf import settings
 from paying_for_college.views import LandingView
 from django.contrib import admin
-# from django.conf.urls.static import static
+from django.conf import settings
+try:
+    STANDALONE = settings.STANDALONE
+except AttributeError:  # pragma: no cover
+    STANDALONE = False
 
 urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$',
         LandingView.as_view(), name='pfc-landing'),
     url(r'^compare-financial-aid-and-college-cost/',
-        include('paying_for_college.disclosures.urls', namespace='disclosures')),
+        include('paying_for_college.disclosures.urls',
+                namespace='disclosures')),
     url(r'^repay-student-debt/',
         include('paying_for_college.debt.urls', namespace='debt')),
     url(r'^guides/',
         include('paying_for_college.guides.urls', namespace='guides')),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL,
-#                           document_root=settings.STATIC_ROOT)
+if STANDALONE:
+    urlpatterns += [
+    url(r'^paying-for-college/$',
+        LandingView.as_view(), name='standalone:pfc-landing'),
+    url(r'^paying-for-college/compare-financial-aid-and-college-cost/',
+        include('paying_for_college.disclosures.urls',
+                namespace='standalone-disclosures')),
+    url(r'^paying-for-college/repay-student-debt/',
+        include('paying_for_college.debt.urls', namespace='standalone-debt')),
+    url(r'^paying-for-college/guides/',
+        include('paying_for_college.guides.urls', namespace='standalone-guides')),
+
+    ]
--- paying_for_college/config/wsgi.py
+++ paying_for_college/config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.test")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "paying_for_college.config.settings.test")
 
 application = get_wsgi_application()
