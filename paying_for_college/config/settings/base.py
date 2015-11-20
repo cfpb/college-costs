40443f2840de6130e956771140eda92eec0b0ee8
higs4281 <higs4281@gmail.com>
2015-11-20 17:58:55 -0500

    remove lovely django 1.8-specific middleware

--- paying_for_college/config/settings/base.py
+++ paying_for_college/config/settings/base.py
@@ -44,10 +44,10 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'paying_for_college.config.urls'
