199c9e2a62e7a69dd3573a88250d2a3d6a46f515
Chuck Werner <mistergone@gmail.com>
2015-11-20 16:28:13 -0500

    Basic test file added

---
+++ test/js/test.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe( 'A test...', function() {
+
+  it( '...knows that 1=1.', function() {
+    expect( 1 ).to.equal( 1 );
+  });
+
+});
