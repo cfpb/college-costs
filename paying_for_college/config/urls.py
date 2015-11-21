a978f15420f60d56391316406278bef3e53b3a1d
higs4281 <higs4281@gmail.com>
2015-11-20 22:44:14 -0500

    new url scheme

--- paying_for_college/config/urls.py
+++ paying_for_college/config/urls.py
@@ -1,36 +1,46 @@
 from django.conf.urls import url, include
 from django.conf import settings
-from paying_for_college.views import LandingView
+from paying_for_college.views import LandingView, StandAloneView
 from django.contrib import admin
 from django.conf import settings
+
 try:
     STANDALONE = settings.STANDALONE
 except AttributeError:  # pragma: no cover
     STANDALONE = False
 
 urlpatterns = [
-    # url(r'^admin/', include(admin.site.urls)),
     url(r'^$',
         LandingView.as_view(), name='pfc-landing'),
-    url(r'^compare-financial-aid-and-college-cost/',
+    url(r'^understanding-financial-aid-offers/',
         include('paying_for_college.disclosures.urls',
                 namespace='disclosures')),
-    url(r'^repay-student-debt/',
-        include('paying_for_college.debt.urls', namespace='debt')),
-    url(r'^guides/',
-        include('paying_for_college.guides.urls', namespace='guides')),
+    url(r'^repaying-student-debt/$',
+        StandAloneView.as_view(template_name='repay_student_debt.html'),
+        name='pfc-repay'),
+    url(r'^choosing-a-student-loan/$',
+        StandAloneView.as_view(template_name='choose_a_loan.html'),
+        name='pfc-choose'),
+    url(r'^managing-college-money/$',
+        StandAloneView.as_view(template_name='manage_your_money.html'),
+        name='pfc-manage'),
 ]
 
 if STANDALONE:
     urlpatterns += [
+    url(r'^paying-for-college/admin/', include(admin.site.urls)),
     url(r'^paying-for-college/$',
         LandingView.as_view(), name='standalone:pfc-landing'),
     url(r'^paying-for-college/compare-financial-aid-and-college-cost/',
         include('paying_for_college.disclosures.urls',
                 namespace='standalone-disclosures')),
-    url(r'^paying-for-college/repay-student-debt/',
-        include('paying_for_college.debt.urls', namespace='standalone-debt')),
-    url(r'^paying-for-college/guides/',
-        include('paying_for_college.guides.urls', namespace='standalone-guides')),
-
+    url(r'^paying-for-college/repaying-student-debt/',
+        StandAloneView.as_view(template_name='repay_student_debt.html'),
+        name='standalone-pfc-repay'),
+    url(r'^paying-for-college/choosing-a-student-loan/$',
+        StandAloneView.as_view(template_name='choose_a_loan.html'),
+        name='standalone-pfc-choose'),
+    url(r'^paying-for-college/managing-college-money/$',
+        StandAloneView.as_view(template_name='manage_your_money.html'),
+        name='standalone-pfc-manage'),
     ]
