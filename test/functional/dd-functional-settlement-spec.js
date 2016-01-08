3b3542a82342f586bf9e26489a66d33df3575379
Naa Marteki Reed <naamarteki.reed@cfpb.gov>
2016-01-08 11:08:52 -0500

    Fix indentation again, and be more explicit about cost of attendance tests

--- test/functional/dd-functional-settlement-spec.js
+++ test/functional/dd-functional-settlement-spec.js
@@ -3,115 +3,129 @@
 var SettlementPage = require( './settlementAidOfferPage.js' );
 
 describe( 'A dynamic financial aid disclosure that\'s required by settlement', function() {
-	var page;
-	var EC = protractor.ExpectedConditions;
+  var page;
+  var EC = protractor.ExpectedConditions;
 
-	beforeEach(function() {
-		page = new SettlementPage();
-	} );
+  beforeEach(function() {
+    page = new SettlementPage();
+  } );
 
-	it( 'should provide a student with the right page', function() {
-		expect( browser.getTitle() ).toBe( 'Understanding your financial aid offer > Consumer Financial Protection Bureau' );
-	} );
+  it( 'should provide a student with the right page', function() {
+    expect( browser.getTitle() ).toBe( 'Understanding your financial aid offer > Consumer Financial Protection Bureau' );
+  } );
 
-	it( 'should contain an offer ID in the URL', function(){
-		expect( browser.getCurrentUrl() ).toContain( 'oid' );
-	} );
+  it( 'should contain an offer ID in the URL', function(){
+    expect( browser.getCurrentUrl() ).toContain( 'oid' );
+  } );
 
-	it( 'should contain a college ID and a program ID in the URL', function(){
-		expect( browser.getCurrentUrl() ).toContain( 'iped' );
-		expect( browser.getCurrentUrl() ).toContain( 'pid' );
-	} );
+  it( 'should contain a college ID and a program ID in the URL', function(){
+    expect( browser.getCurrentUrl() ).toContain( 'iped' );
+    expect( browser.getCurrentUrl() ).toContain( 'pid' );
+  } );
 
-	it( 'should contain required aid offer values in the URL', function(){
-		expect( browser.getCurrentUrl() ).toContain( 'tuit' );
+  it( 'should contain required aid offer values in the URL', function(){
+    expect( browser.getCurrentUrl() ).toContain( 'tuit' );
     expect( browser.getCurrentUrl() ).toContain( 'hous' );
     expect( browser.getCurrentUrl() ).toContain( 'book' );
-	} );
+  } );
 
-	// TODO - Add expectation that other sections are invisible
-	it( 'should display the verify offer area and no other sections', function() {
-		browser.wait( EC.visibilityOf(page.verifySection ), 8000 );
-	} );
+  // TODO - Add expectation that other sections are invisible
+  it( 'should display the verify offer area and no other sections', function() {
+    browser.wait( EC.visibilityOf(page.verifySection ), 8000 );
+  } );
 
-	// TODO - Add expectation that verification buttons disappear, and all fields that should be prepopulated actually are, and that Step 3 is still hidden
-	it( 'should let a student verify their information and go on to Step 1 and Step 2 of the offer', function() {
-		page.confirmVerification();
-		browser.wait( EC.visibilityOf(page.reviewSection ), 8000 );
-		browser.wait( EC.visibilityOf(page.evaluateSection ), 8000 );
-	} );
+  // TODO - Add expectation that verification buttons disappear, and all fields that should be prepopulated actually are, and that Step 3 is still hidden
+  it( 'should let a student verify their information and go on to Step 1 and Step 2 of the offer', function() {
+    page.confirmVerification();
+    browser.wait( EC.visibilityOf(page.reviewSection ), 8000 );
+    browser.wait( EC.visibilityOf(page.evaluateSection ), 8000 );
+  } );
 
-	// TODO - Add expectation that verification buttons disappear, offer ssections are not visible, that next steps for incorrect info are displayed, and that the trigger to notify the school is activated
-	it( 'should let a student report incorrect aid offer information', function() {
-		page.denyVerification();
-	} );
+  // TODO - Add expectation that verification buttons disappear, offer sections are not visible, that next steps for incorrect info are displayed, and that the trigger to notify the school is activated
+  it( 'should let a student report incorrect aid offer information', function() {
+    page.denyVerification();
+  } );
 
-	// *** Step 1: Review your offer ***
+  // *** Step 1: Review your offer ***
   // TODO: After the offer URL implementation conforms to spec, 
   // we should draw values straight from the page based on URL values
   // rather than setting all of them explicitly here.
 
-	// TODO - Uncomment remaining cost when it's hooked up
-	it( 'should properly update when the tuition is modified', function() {
-		page.confirmVerification();
-		page.setTuitionFeesCosts( 3500 );
-		expect(page.totalCostOfAttendance.getText()).toEqual( '3500' );
-		expect(page.studentTotalCost.getText()).toEqual( '3500' );
-		//expect(page.remainingCost.getText()).toEqual( '3500' );
-	} );
+  // Cost of attendance
 
-	// TODO - Uncomment remaining cost when it's hooked up
-	it( 'should properly update when the housing and meals are modified', function() {
-		page.confirmVerification();
-		page.setTuitionFeesCosts( 3500 );
-		page.setHousingMealsCosts( 1500 );
-		expect(page.totalCostOfAttendance.getText()).toEqual( '5000' );
-		expect(page.studentTotalCost.getText()).toEqual( '5000' );
-		//expect(page.addUpRemainingCost()).toEqual(page.remainingCost.getText());
-	} );
+  it( 'should properly update when the tuition is modified', function() {
+    page.confirmVerification();
+    page.setTuitionFeesCosts( 38976 );
+    expect(page.totalCostOfAttendance.getText()).toEqual( '38976' );
+    expect(page.studentTotalCost.getText()).toEqual( '38976' );
+    expect(page.remainingCost.getText()).toEqual( '38976' );
+  } );
 
-	// TODO - Uncomment remaining cost when it's hooked up
-	it( 'should properly update when the transportation is modified', function() {
-		page.confirmVerification();
-		page.setTuitionFeesCosts( 3500 );
-		page.setHousingMealsCosts( 1500 );
-		page.setTransportationCosts( 500 );
-		expect(page.totalCostOfAttendance.getText()).toEqual( '5500' );
-		expect(page.studentTotalCost.getText()).toEqual( '5500' );
-		//expect(page.addUpRemainingCost()).toEqual(page.remainingCost.getText());
-	} );
+  it( 'should properly update when the housing and meals are modified', function() {
+    page.confirmVerification();
+    page.setTuitionFeesCosts( 38976 );
+    page.setHousingMealsCosts( 3000 );
+    expect(page.totalCostOfAttendance.getText()).toEqual( '41976' );
+    expect(page.studentTotalCost.getText()).toEqual( '41976' );
+    expect(page.remainingCost.getText()).toEqual( '41976' );
+  } );
 
-	// TODO - Uncomment remaining cost when it's hooked up
-	it( 'should properly update when the books and supplies are modified', function() {
-		page.confirmVerification();
-		page.setTuitionFeesCosts( 3500 );
-		page.setHousingMealsCosts( 1500 );
-		page.setTransportationCosts( 500 );
-		page.setBooksSuppliesCosts( 500 );
-		expect(page.totalCostOfAttendance.getText()).toEqual( '6000' );
-		expect(page.studentTotalCost.getText()).toEqual( '6000' );
-		//expect(page.addUpRemainingCost()).toEqual(page.remainingCost.getText());
-	} );
+  it( 'should properly update when the transportation is modified', function() {
+    page.confirmVerification();
+    page.setTuitionFeesCosts( 38976 );
+    page.setHousingMealsCosts( 3000 );
+    page.setTransportationCosts( 500 );
+    expect(page.totalCostOfAttendance.getText()).toEqual( '42476' );
+    expect(page.studentTotalCost.getText()).toEqual( '42476' );
+    expect(page.remainingCost.getText()).toEqual( '42476' );
+  } );
 
-	// TODO - Uncomment remaining cost when it's hooked up
-	it( 'should properly update when the other education costs are modified', function() {
-		page.confirmVerification();
-		page.setTuitionFeesCosts( 3500 );
-		page.setHousingMealsCosts( 1500 );
-		page.setOtherEducationCosts( 250 );
-		expect(page.totalCostOfAttendance.getText()).toEqual( '5250' );
-		expect(page.studentTotalCost.getText()).toEqual( '5250' );
-		//expect(page.addUpRemainingCost()).toEqual(page.remainingCost.getText());
-	} );
+  it( 'should properly update when the books and supplies are modified', function() {
+    page.confirmVerification();
+    page.setTuitionFeesCosts( 38976 );
+    page.setHousingMealsCosts( 3000 );
+    page.setTransportationCosts( 500 );
+    page.setBooksSuppliesCosts( 650 );
+    expect(page.totalCostOfAttendance.getText()).toEqual( '43126' );
+    expect(page.studentTotalCost.getText()).toEqual( '43126' );
+    expect(page.remainingCost.getText()).toEqual( '43126' );
+  } );
 
-	// TODO - Uncomment remaining cost when it's hooked up
-	it( 'should properly update when the Federal Pell Grants are modified within the limits', function() {
-		page.confirmVerification();
-		page.setTuitionFeesCosts( 15000 );
-		page.setFederalPellGrants( 5000 );
-		expect(page.totalGrantsScholarships.getText()).toEqual( '5000' );
-		expect(page.studentTotalCost.getText()).toEqual( '10000' );
-		//expect(page.addUpRemainingCost()).toEqual(page.remainingCost.getText());
-	} );
+  it( 'should properly update when the other education costs are modified', function() {
+    page.confirmVerification();
+    page.setTuitionFeesCosts( 38976 );
+    page.setHousingMealsCosts( 3000 );
+    page.setTransportationCosts( 500 );
+    page.setBooksSuppliesCosts( 650 );
+    page.setOtherEducationCosts( 500 );
+    expect(page.totalCostOfAttendance.getText()).toEqual( '43626' );
+    expect(page.studentTotalCost.getText()).toEqual( '43626' );
+    expect(page.remainingCost.getText()).toEqual( '43626' );
+  } );
+
+  // Grants and scholarships
+
+  it( 'should properly update when the Federal Pell Grants are modified within the limits', function() {
+    page.confirmVerification();
+    page.setTuitionFeesCosts( 38976 );
+    page.setHousingMealsCosts( 3000 );
+    page.setTransportationCosts( 500 );
+    page.setBooksSuppliesCosts( 650 );
+    page.setOtherEducationCosts( 500 );
+    page.setFederalPellGrants( 5000 );
+    expect(page.totalGrantsScholarships.getText()).toEqual( '5000' );
+    expect(page.studentTotalCost.getText()).toEqual( '38626' );
+    expect(page.remainingCost.getText()).toEqual( '38626' );
+  } );
+
+  // Personal and family contributions
+
+  // Federal loans
+
+  // Private loans and payment plans
+
+  // Big picture summary
+
+  // Debt summary
 
 });
