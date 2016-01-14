9ff3a577a60a778089373f0d89b8f89a92b04b18
Adam Scott <adam.scott@cfpb.gov>
2016-01-14 09:59:10 -0500

    so long sav

--- src/disclosures/js/utils/query-handler.js
+++ src/disclosures/js/utils/query-handler.js
@@ -41,8 +41,7 @@ function queryHandler( queryString ) {
     prvl: 'privateLoan',
     prvi: 'privateLoanRate',
     insl: 'institutionalLoan',
-    insi: 'institutionalLoanRate',
-    sav:  'savings'
+    insi: 'institutionalLoanRate'
   };
 
   function checkValue( key, value ) {
