fc73df84eb32b893a578b5c13bea81d5472ecf92
Chuck Werner <mistergone@gmail.com>
2016-02-05 15:48:36 -0500

    Added JSDoc comments for publish-update.js functions

--- src/disclosures/js/dispatchers/publish-update.js
+++ src/disclosures/js/dispatchers/publish-update.js
@@ -3,11 +3,23 @@
 var financialModel = require( '../models/financial-model' );
 
 var publishUpdate = {
+
+  /**
+   * Function which updates financial model with new value
+   * @param {string} prop - financial model property name
+   * @param {number|string} val - new value
+   */
   financialData: function( prop, val ) {
     financialModel.values[prop] = val;
     financialModel.calc( financialModel.values );
   },
 
+  /**
+   * Function which updates privateLoanMulti array in financial model with new value
+   * @param {number} index - The index of the private loan being updated
+   * @param {string} prop - private loan object property name
+   * @param {number|string} val - new value
+   */
   updatePrivateLoan: function( index, prop, val ) {
     financialModel.values.privateLoanMulti[index][prop] = val;
     financialModel.values.privateLoanMulti[index].amount =
@@ -16,11 +28,18 @@ var publishUpdate = {
     financialModel.calc( financialModel.values );
   },
 
+  /**
+   * Function which removes a private loan from the privateLoanMulti array
+   * @param {number} index - The index of the private loan being removed
+   */
   dropPrivateLoan: function( index ) {
     financialModel.values.privateLoanMulti.splice( index, 1 );
     financialModel.calc( financialModel.values );
   },
 
+  /**
+   * Function which adds a private loan object to the privateLoanMulti array in financial model
+   */
   addPrivateLoan: function() {
     var newLoanObject = { amount: 0,
                           baseAmount: 0,
