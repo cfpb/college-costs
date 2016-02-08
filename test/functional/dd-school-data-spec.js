5bdd37170e4e22f20862d73c2d5d790f0e784a08
Naa Marteki Reed <naamarteki.reed@cfpb.gov>
2016-02-08 13:06:34 -0500

    Add functional test for program length selector

---
+++ test/functional/dd-school-data-spec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var SettlementPage = require( './settlementAidOfferPage.js' );
+
+fdescribe( 'The dynamic financial aid disclosure', function() {
+  var page;
+  var EC = protractor.ExpectedConditions;
+
+  beforeEach(function() {
+    page = new SettlementPage();
+  } );
+
+  it( 'should automatically populate the program length if it\'s available', function() {
+     browser.sleep( 600 );
+     expect( page.programLengthSelect.$('option:checked').getText() ).toMatch( /2 years/ );
+  } );
+
+} );
