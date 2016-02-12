446cc09fc60d15a0c25b964ec4428726bf7ac05e
Naa Marteki Reed <naamarteki.reed@cfpb.gov>
2016-02-12 09:46:48 -0500

    Monthly expenses and "program length in words"

--- src/disclosures/js/dispatchers/get-school-values.js
+++ src/disclosures/js/dispatchers/get-school-values.js
@@ -1,15 +1,20 @@
 'use strict';
 
+var numberToWords = require( 'number-to-words' );
+
 var getSchoolValues = {
 
   init: function( ) {
     var values = {};
 
     values.programLength = this.getProgramLength();
+    values.yearsAttending = numberToWords.toWords( values.programLength );
     values.gradRate = this.getGradRate();
     values.medianSchoolDebt = this.getMedianSchoolDebt();
     values.defaultRate = this.getDefaultRate();
     values.medianSalary = this.getMedianSalary();
+    values.monthlySalary = this.setMonthlySalary( values.medianSalary );
+    values = this.getBLSExpenses( values );
     values.jobRate = this.getJobRate();
 
     return values;
@@ -112,6 +117,50 @@ var getSchoolValues = {
     }
 
     return medianSalary;
+  },
+
+  setMonthlySalary: function( medianSalary ) {
+    var monthlySalary;
+
+    if ( medianSalary === '' ) {
+      monthlySalary = 0;
+    } else {
+      monthlySalary = Math.round( Number( medianSalary ) / 12 ).toFixed( 0 );
+    }
+
+    return monthlySalary;
+  },
+
+  getBLSExpenses: function( values ) {
+
+    // BLS expense data is delivered as annual values.
+    // The tool displays monthly expenses.
+
+    if ( window.hasOwnProperty( 'nationalData' ) ) {
+      if ( window.nationalData.region === 'Not available' ) {
+        values.BLSAverage = 'national';
+        values.monthlyRent = window.nationalData.nationalHousing / 12;
+        values.monthlyFood = window.nationalData.nationalFood / 12;
+        values.monthlyTransportation =
+          window.nationalData.nationalTransportation / 12;
+        values.monthlyInsurance = window.nationalData.nationalHealthcare / 12;
+        values.monthlySavings = window.nationalData.nationalRetirement / 12;
+        values.monthlyOther =
+          window.nationalData.nationalEntertainment / 12;
+      } else {
+        values.BLSAverage = window.nationalData.region + ' regional';
+        values.monthlyRent = window.nationalData.regionalHousing / 12;
+        values.monthlyFood = window.nationalData.regionalFood / 12;
+        values.monthlyTransportation =
+          window.nationalData.regionalTransportation / 12;
+        values.monthlyInsurance = window.nationalData.regionalHealthcare / 12;
+        values.monthlySavings = window.nationalData.regionalRetirement / 12;
+        values.monthlyOther =
+          window.nationalData.regionalEntertainment / 12;
+      }
+    }
+
+    return values;
   }
 
 };
