2f54f101afb5f38e3ea09f883cc3195ad88b578a
Chuck Werner <mistergone@gmail.com>
2016-02-17 14:42:49 -0500

    Fixes for no-offer mode

--- src/disclosures/js/views/links-view.js
+++ src/disclosures/js/views/links-view.js
@@ -40,13 +40,15 @@ var linksView = {
   },
 
   setScorecardSearch: function() {
-    var pcip = window.programData.cipCode ? window.programData.cipCode.slice( 0, 2 ) : '',
-        zip = window.schoolData.zip5 || '',
-        // We're using a 50-mile radius, the most common Scorecard search
-        radius = '50',
-        scorecardURL = this.$scorecardLink.attr( 'href' ),
-        scorecardQuery = constructScorecardSearch( pcip, zip, radius );
-    this.$scorecardLink.attr( 'href', scorecardURL + scorecardQuery );
+    if ( window.hasOwnProperty( 'programData' ) ) {
+      var pcip = window.programData.cipCode ? window.programData.cipCode.slice( 0, 2 ) : '',
+          zip = window.schoolData.zip5 || '',
+          // We're using a 50-mile radius, the most common Scorecard search
+          radius = '50',
+          scorecardURL = this.$scorecardLink.attr( 'href' ),
+          scorecardQuery = constructScorecardSearch( pcip, zip, radius );
+      this.$scorecardLink.attr( 'href', scorecardURL + scorecardQuery );
+    }
   }
 
 };
