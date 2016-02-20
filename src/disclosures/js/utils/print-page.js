993b944177287009df3b02cf7128ecc5bc53dd7c
Naa Marteki Reed <naamarteki.reed@cfpb.gov>
2016-02-19 19:55:53 -0500

    Add Google Cloud Print support

--- src/disclosures/js/utils/print-page.js
+++ src/disclosures/js/utils/print-page.js
@@ -1,28 +1,27 @@
 'use strict';
 
+var androidPrint = require( '../libs/google-cloud-print' );
+var ua = navigator.userAgent.toLowerCase(),
+    isAndroid = ua.indexOf( 'android' ) > -1;
+
 /**
- * Print the current screen.
- * Uses Google Cloud Print for Android devices.
+ * Print the current screen using Google Cloud Print for Android devices.
  */
-function printPage() {
-  var ua = navigator.userAgent.toLowerCase(),
-      isAndroid = ua.indexOf( 'android' ) > -1;
-
-  if ( isAndroid ) {
-    // https://developers.google.com/cloud-print/docs/gadget
-    /* var gadget = new cloudprint.Gadget();
-    gadget.setPrintDocument( 'url', $( 'title' ).html(), window.location.href,
-      'utf-8' );
-    gadget.openPrintDialog(); */
-  } else {
-    window.print();
-  }
-
+function printAndroidPage() {
+  // https://developers.google.com/cloud-print/docs/gadget
+  var gadget = new androidPrint.cloudprint.Gadget();
+  gadget.setPrintDocument( 'url', $( 'title' ).html(), window.location.href,
+    'utf-8' );
+  gadget.openPrintDialog();
 }
 
 $( document ).ready( function() {
   $( '.next-steps_controls > .btn' ).on( 'click', function( e ) {
     e.preventDefault();
-    printPage();
+    if ( isAndroid ) {
+      printAndroidPage();
+    } else {
+      window.print();
+    }
   } );
 } );
