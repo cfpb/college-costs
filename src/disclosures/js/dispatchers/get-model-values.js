fe1b8cd8e9fb284df230d996b3316429bddfb8eb
Chuck Werner <mistergone@gmail.com>
2016-03-10 15:49:10 -0500

    Attempting to send error to /api/verify/

--- src/disclosures/js/dispatchers/get-model-values.js
+++ src/disclosures/js/dispatchers/get-model-values.js
@@ -1,10 +1,14 @@
 'use strict';
 
 var financialModel = require( '../models/financial-model' );
+var schoolModel = require( '../models/school-model' );
 
 var getModel = {
   financial: function() {
     return financialModel.values;
+  },
+  school: function() {
+    return schoolModel.values;
   }
 };
 
--- src/disclosures/js/dispatchers/post-verify.js
+++ src/disclosures/js/dispatchers/post-verify.js
@@ -4,19 +4,33 @@ var postVerify = {
   csrfToken: null,
 
   init: function() {
-    if ( document.cookie && document.cookie != '' ) {
-      var cookies = document.cookie.split( ';' );
-      for ( var x = 0; x < cookes.length; x++ ) {
-        var cookie = $.trim( cookies[ x ] );
-        if ( cookie.substring( 0, 10 ) === 'csrftoken=' ) {
-          this.csrfToken = cookie.substring( 10 );
-          break;
-        }
-      }
-    }
+    // Alternate code. getting token from document.cookie:
+    // 
+    // if ( document.cookie && document.cookie != '' ) {
+    //   var cookies = document.cookie.split( ';' );
+    //   for ( var x = 0; x < cookes.length; x++ ) {
+    //     var cookie = $.trim( cookies[ x ] );
+    //     if ( cookie.substring( 0, 10 ) === 'csrftoken=' ) {
+    //       this.csrfToken = cookie.substring( 10 );
+    //       break;
+    //     }
+    //   }
+    // }
+    this.csrfToken = $('[name="csrfmiddlewaretoken"]').val();
   },
 
-
+  verify: function( offerID, collegeID, error ) {
+    var postdata = {
+      'csrfmiddlewaretoken':  this.csrfToken,
+      'oid':                  offerID,
+      'iped':                 collegeID,
+      'errors':               'none'
+    };
+    if ( error === true ) {
+      postdata.errors = 'INVALID: student indicated the offer information is wrong';
+    }
+    $.post( '/understanding-your-financial-aid-offer/api/verify/', postdata );
+  }
 
 };
 
