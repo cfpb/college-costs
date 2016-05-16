4c1a62028dc0dddaa47600f4bffe484a76398a6e
Naa Marteki Reed <naamarteki.reed@cfpb.gov>
2016-05-16 11:19:14 -0400

    Remove unneeded NPM install line

--- setup.sh
+++ setup.sh
@@ -27,7 +27,6 @@ clean(){
 install(){
   echo 'Installing project dependencies...'
   npm install
-  npm install --save --save-exact box-sizing-polyfill@0.1.0 jquery.easing@1.3.2 normalize-css@2.3.1 jquery@1.11.3 normalize-legacy-addon@0.1.0
   if [ -f $SHRINKWRAP ]; then
     echo 'Removing project dependency directories...'
     rm -f $SHRINKWRAP
