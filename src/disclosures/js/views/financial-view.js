b978a2fd41f7eddad0b078e94c793fa16b41f43c
Naa Marteki Reed <naamarteki.reed@cfpb.gov>
2016-05-16 22:55:58 -0400

    Add "Continue to Step 2" interaction right after Step 1

--- src/disclosures/js/views/financial-view.js
+++ src/disclosures/js/views/financial-view.js
@@ -7,7 +7,7 @@ var formatUSD = require( 'format-usd' );
 var numberToWords = require( 'number-to-words' );
 var linksView = require( '../views/links-view' );
 var metricView = require( '../views/metric-view' );
-var postVerification = require( '../dispatchers/post-verify');
+var postVerification = require( '../dispatchers/post-verify' );
 
 var financialView = {
   $elements: $( '[data-financial]' ),
@@ -17,10 +17,12 @@ var financialView = {
   $infoIncorrect: $( '.information-wrong' ),
   $programLength: $( '#estimated-years-attending' ),
   $addPrivateButton: $( '.private-loans_add-btn' ),
-  $gradPlusSection: $( '[data-section="gradPlus"]'),
+  $gradPlusSection: $( '[data-section="gradPlus"]' ),
   $privateContainer: $( '.private-loans' ),
   $privateLoanClone: $( '[data-private-loan]:first' ).clone(),
   privateLoanKeys: [ 'amount', 'fees', 'rate', 'deferPeriod' ],
+  $evaluateSection: $( '.evaluate' ),
+  $bigQuestion: $( '.question' ),
   keyupDelay: null,
   currentInput: null,
 
@@ -35,13 +37,14 @@ var financialView = {
     this.addPrivateListener();
     this.removePrivateListener();
     this.resetPrivateLoanView();
+    this.continueStep2Listener();
   },
 
   /**
    * Helper function that updates the value or text of an element
    * @param {object} $ele - jQuery object of the element to update
    * @param {number|string} value - The new value
-   * @param {Boolean} currency - True if the value is to be formatted as currency
+   * @param {Boolean} currency - True if value is to be formatted as currency
    */
   updateElement: function( $ele, value, currency ) {
     if ( currency === true ) {
@@ -72,7 +75,8 @@ var financialView = {
   },
 
   /**
-   * Helper function that updates all non-percent, non-privateLoan elements in the financial view
+   * Helper function that updates all non-percent, non-privateLoan elements
+   * in the financial view
    * @param {object} values - financial model values
    * @param {object} $leftovers - jQuery object of the "leftover" elements
    */
@@ -103,7 +107,8 @@ var financialView = {
       $fields.not( '#' + financialView.currentInput ).each( function() {
         var key = $( this ).attr( 'data-private-loan_key' ),
             val = values.privateLoanMulti[index][key],
-            isntCurrentInput = $( this ).attr( 'id' ) !== financialView.currentInput;
+            isntCurrentInput =
+            $( this ).attr( 'id' ) !== financialView.currentInput;
         if ( $( this ).is( '[data-percentage_value="true"]' ) ) {
           val *= 100;
           $( this ).val( val );
@@ -151,8 +156,8 @@ var financialView = {
   },
 
   /**
-   * Updates view based on program data (including school data). This updates the
-   * programLength dropdown and visibility of gradPLUS loans.
+   * Updates view based on program data (including school data).
+   * This updates the programLength dropdown and visibility of gradPLUS loans.
    * @param {object} values - An object with program values
    */
   updateViewWithProgram: function( values ) {
@@ -325,6 +330,20 @@ var financialView = {
     } else {
       this.$gradPlusSection.show();
     }
+  },
+
+  continueStep2Listener: function() {
+    var $continueButton = $( '.continue_controls > .btn' );
+    $continueButton.on( 'click', function() {
+      // Remove continue button
+      $continueButton.hide();
+      // Show Step 2
+      financialView.$evaluateSection.show();
+      financialView.$bigQuestion.show();
+      $( 'html, body' ).stop().animate( {
+        scrollTop: financialView.$evaluateSection.offset().top - 120
+      }, 900, 'swing', function() {} );
+    } );
   }
 };
 
