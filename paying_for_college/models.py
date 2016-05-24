23b14408933c1338340ff28c859f16de64067cf4
higs4281 <higs4281@gmail.com>
2016-05-24 16:09:05 -0400

    adjust wording for highest-degree and predominant-degree to match cleared content

--- paying_for_college/models.py
+++ paying_for_college/models.py
@@ -31,7 +31,7 @@ REGION_NAMES = {'MW': 'Midwest',
 
 HIGHEST_DEGREES = {  # highest-awarded values from Ed API and our CSV spec
     '0': "Non-degree-granting",
-    '1': 'Certificate degree',
+    '1': 'Certificate',
     '2': "Associate degree",
     '3': "Bachelor's degree",
     '4': "Graduate degree"
