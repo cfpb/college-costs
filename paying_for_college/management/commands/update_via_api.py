6bf25a64045d10e18eca196b5db425807158f6c8
higs4281 <higs4281@gmail.com>
2016-06-06 09:29:16 -0400

    Add a manage.py command for updating colleges
    Exposing the script for updating college data via the collegeScorecard
    API will make it easier for Jenkins or other automation tools to update
    school data. The `update_via_api` command will attempt to update every
    school in our database based on the `latest_year` value stored as a
    constant in our database.
    
    An optional flag of `--school_id` will limit an update to just one
    school, which is useful for testing, or for fixing an error found with a
    single school.

--- paying_for_college/management/commands/update_via_api.py
+++ paying_for_college/management/commands/update_via_api.py
@@ -3,22 +3,28 @@ import datetime
 from django.core.management.base import BaseCommand, CommandError
 from paying_for_college.disclosures.scripts import update_colleges
 
-COMMAND_HELP = """Updates our School table with values from the Department of \
+COMMAND_HELP = """update_via_api gets school-level data from the Department of \
 Education's CollegeScorecard API. The script intentionally runs slowly \
 to avoid triggering API rate limits, so allow an hour to run."""
-PARSER_HELP = """Optionally specify a single school to update. \
-Pass '--single-school' and a school ID to limit updates to one school."""
+PARSER_HELP = """Optionally specify a single school to update \
+by passing '--school_id' and a college IPEDS ID."""
+ID_ERROR = "School could not be found for ID {}"
 
 
 class Command(BaseCommand):
     help = COMMAND_HELP
 
     def add_arguments(self, parser):
-        parser.add_argument('--single-school',
-                            help=PARSER_HELP)
+        parser.add_argument('--school_id',
+                            help=PARSER_HELP,
+                            default=False)
 
     def handle(self, *args, **options):
-        (failed,
-         no_data,
-         endmsg) = update_colleges.update(options['single-school'])
-        self.stdout.write(endmsg)
+        try:
+            (failed,
+             no_data,
+             endmsg) = update_colleges.update(single_school=options['school_id'])
+        except(IndexError):
+            self.stdout.write(ID_ERROR.format(options['school_id']))
+        else:
+            self.stdout.write(endmsg)
--- paying_for_college/tests/test_commands.py
+++ paying_for_college/tests/test_commands.py
@@ -8,13 +8,10 @@ from paying_for_college.management.commands import update_via_api
 
 
 class CommandTests(unittest.TestCase):
-    @mock.patch('paying_for_college.management.commands.update_colleges.update')
+    @mock.patch('paying_for_college.management.commands.update_via_api.update_colleges.update')
     def test_api_update(self, mock_update):
         mock_update.return_value = ([], [], 'OK')
-        (f, n, test_run) = call_command('update_via_api')
+        call_command('update_via_api')
         self.assertTrue(mock_update.call_count == 1)
-        self.assertTrue('OK' in test_run)
-        (f2, n2, test_run2) = call_command('update_via_api',
-                                           '--single-school 99999')
-        self.assertTrue('OK' in test_run2)
+        call_command('update_via_api', '--school_id', '99999')
         self.assertTrue(mock_update.call_count == 2)
