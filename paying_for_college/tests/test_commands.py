92bf6ed089e5d4d7a21170c2fbaeb25d4aefd53f
Amy Mok <amy.mok@cfpb.gov>
2016-06-13 16:35:11 -0400

    Add test for load_programs command

--- paying_for_college/tests/test_commands.py
+++ paying_for_college/tests/test_commands.py
@@ -4,7 +4,7 @@ import unittest
 from django.core.management.base import CommandError
 from django.core.management import call_command
 
-from paying_for_college.management.commands import update_via_api
+from paying_for_college.management.commands import update_via_api, load_programs
 
 
 class CommandTests(unittest.TestCase):
@@ -15,3 +15,11 @@ class CommandTests(unittest.TestCase):
         self.assertTrue(mock_update.call_count == 1)
         call_command('update_via_api', '--school_id', '99999')
         self.assertTrue(mock_update.call_count == 2)
+
+    @mock.patch('paying_for_college.management.commands.load_programs.load_programs.load')
+    def test_load_programs(self, mock_load):
+        mock_load.return_value = ([], 'OK')
+        call_command('load_programs', 'filename')
+        self.assertEqual(mock_load.call_count, 1)
+        mock_load.assert_called_once_with('filename')
+
