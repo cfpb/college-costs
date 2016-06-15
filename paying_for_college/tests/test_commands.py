4cd457e07e60cdae528a6ded677e646d45436d54
Amy Mok <amy.mok@cfpb.gov>
2016-06-15 12:43:07 -0400

    Test with more files

--- paying_for_college/tests/test_commands.py
+++ paying_for_college/tests/test_commands.py
@@ -23,3 +23,10 @@ class CommandTests(unittest.TestCase):
         self.assertEqual(mock_load.call_count, 1)
         mock_load.assert_called_once_with('filename')
 
+    @mock.patch('paying_for_college.management.commands.load_programs.load_programs.load')
+    def test_load_programs_more_than_1_files(self, mock_load):
+        mock_load.return_value = ([], 'OK')
+        call_command('load_programs', 'filename', 'filename2', 'filename3')
+        self.assertEqual(mock_load.call_count, 3)
+        mock_load.assert_has_calls([mock.call('filename'), mock.call('filename2'), mock.call('filename3')])
+
