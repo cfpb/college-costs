68c3d78dee66496fa7be040e5e6dbb73ef0fac2e
Naa Marteki Reed <naamarteki.reed@cfpb.gov>
2016-06-27 14:50:13 -0400

    Adjust setup.sh so that the shrink-wrap file is removed before installation is attempted, and re-shrinkwrap the dependencies

--- setup.sh
+++ setup.sh
@@ -26,12 +26,12 @@ clean(){
 # Install project dependencies.
 install(){
   echo 'Installing project dependencies...'
-  npm install
-  npm install --save --save-exact box-sizing-polyfill@0.1.0 jquery.easing@1.3.2 normalize-css@2.3.1 jquery@1.11.3 normalize-legacy-addon@0.1.0
   if [ -f $SHRINKWRAP ]; then
-    echo 'Removing project dependency directories...'
+    echo 'Removing previous shrinkwrapped dependency settings...'
     rm -f $SHRINKWRAP
   fi
+  npm install
+  npm install --save --save-exact box-sizing-polyfill@0.1.0 jquery.easing@1.3.2 normalize-css@2.3.1 jquery@1.11.3 normalize-legacy-addon@0.1.0
   echo 'Shrinkwrapping project dependencies...'
   npm shrinkwrap
 }
