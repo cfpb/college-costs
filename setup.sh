d15ae404c2f42106bab17b25c25033d96421fd03
Naa Marteki Reed <naamarteki.reed@cfpb.gov>
2016-07-15 11:50:28 -0400

    Update NPM packages, and add a `npm prune` before attempting to shrinkwrap. IMPORTANT: remove .snyk from .gitignore, so everyone shares the same snaky security policy file!

--- setup.sh
+++ setup.sh
@@ -33,7 +33,8 @@ install(){
   npm install
   npm install --save --save-exact box-sizing-polyfill@0.1.0 jquery.easing@1.3.2 normalize-css@2.3.1 jquery@1.11.3 normalize-legacy-addon@0.1.0
   echo 'Shrinkwrapping project dependencies...'
-  npm shrinkwrap
+  npm prune
+  npm shrinkwrap --dev
 }
 
 # Run tasks to build the project for distribution.
