c85f52a00cc09a6abf861f7403b0ad7dcdeedbd0
Chuck Werner <mistergone@gmail.com>
2016-07-21 15:28:16 -0400

    This commit fixes the following issues:
    - Tuition payment plan is now included in monthly payments
    - Tuition payment plan is correctly added to totalProgramDebt

--- src/disclosures/js/models/financial-model.js
+++ src/disclosures/js/models/financial-model.js
@@ -61,15 +61,17 @@ var financialModel = {
       model.privateLoanTotal += model.privateLoanMulti[x].amount;
     }
 
-    model.costAfterGrants = model.costOfAttendance - model.grantsTotal;
-    model.totalProgramDebt = model.borrowingTotal * model.programLength;
-
     // Modify values using tuition repayment plan values
     model.tuitionRepayYearly = model.tuitionRepay / model.programLength;
     model.borrowingTotal += model.tuitionRepayYearly;
     model.gap -= model.tuitionRepayYearly;
     model.totalDebt += model.tuitionRepayDebt;
     model.loanLifetime += model.tuitionRepayMonthly * model.tuitionRepayTerm;
+    model.loanMonthly += model.tuitionRepayMonthly;
+
+    // Calculate totals
+    model.costAfterGrants = model.costOfAttendance - model.grantsTotal;
+    model.totalProgramDebt = model.borrowingTotal * model.programLength;
   },
 
   /**
