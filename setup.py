edcd715786738e8538e124ad18fec7606538f3bd
higs4281 <higs4281@gmail.com>
2017-01-24 17:24:46 -0500

    update version in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read_file(filename):
 
 setup(
     name='college-costs',
-    version='2.3.1',
+    version='2.3.2',
     author='CFPB',
     author_email='tech@cfpb.gov',
     maintainer='cfpb',
